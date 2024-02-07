718d969d13a1e4a6eda378c5b80f4374c393d748
Ali Bulut <95712617+MrAliBulut@users.noreply.github.com>
2024-02-07 16:04:08 +0300

    Added QuickSort

--- Java/Main.java
+++ Java/Main.java
@@ -6,12 +6,12 @@ public class Main {
         Random rand = new Random();
 
         int arraySize = 20;  // Size of the array to be generated
-        int arrayRange = 1000;  // Range of values for the random numbers
-        int[] randomizedArray = new int[arraySize];  // Array to hold the random numbers
+        int arrayRange = 50;  // Range of values for the random numbers
+        int[] Array = new int[arraySize];  // Array to hold the random numbers
 
         // Generating random numbers and populating the array
-        for (int i = 0; i < randomizedArray.length; i++) {
-            randomizedArray[i] = rand.nextInt(arrayRange);
+        for (int i = 0; i < Array.length; i++) {
+            Array[i] = rand.nextInt(arrayRange);
         }
 
         // Creating instances of sorting algorithms
@@ -19,20 +19,23 @@ public class Main {
         Insertion insertion = new Insertion();  
         Bubble bubble = new Bubble(); 
         Merge merge = new Merge();  
+        Quick quick = new Quick(); 
         
         System.out.println("================================================================");
 
         // Printing the original array
-        System.out.println("Array:\n" + Arrays.toString(randomizedArray) + "\n");
+        System.out.println("Array:\n" + Arrays.toString(Array) + "\n");
 
         System.out.println("================================================================");
 
         // Sorting the array in ascending order and printing the result
-        int[] minSorted = merge.minOrder(randomizedArray);
+        //Please change the used object accordingly to desired sort method. in this case we are using QuickSort.
+        int[] minSorted = quick.randomized(Array);
         System.out.println("Ascending Order:\n" + Arrays.toString(minSorted) + "\n");
 
         // Sorting the array in descending order and printing the result
-        int[] maxSorted = merge.maxOrder(randomizedArray);
+        //Please change the used object accordingly to desired sort method. in this case we are using QuickSort.
+        int[] maxSorted = quick.maxOrder(Array);
         System.out.println("Descending Order:\n" + Arrays.toString(maxSorted));
 
         System.out.println("================================================================");
